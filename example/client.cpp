0ab730f6e439280ff6a3f13bbb958d6115632c33
Lennard Berger <lennard.berger@student.uni-tuebingen.de>
2020-03-26 18:59:55 +0100

    Update example with new async operation.

--- example/client.cpp
+++ example/client.cpp
@@ -3,6 +3,14 @@
 #include "libassetbundler.h"
 
 int main() {
-    const char* destination = DownloadMap("http://localhost:8000", "curvedm");
-    std::cout << destination << std::endl;
+    char* servercontent = "http://localhost:8000";
+    char* map = "curvedm";
+    int status = 1;
+    char* zippath = StartDownload(servercontent, map);
+    std::cout << zippath << std::endl;
+    while (status == 1) {
+        std::cout << "Do I get here?" << std::endl;
+        status = GetStatus(zippath);
+    }
+    std::cout << "Downloaded everything" << std::endl;
 }
